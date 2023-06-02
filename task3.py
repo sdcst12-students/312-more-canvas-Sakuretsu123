c542f22a4ca7b66b749fca643f880c2e1421955d
Valerian <valerian210404@gmail.com>
2023-06-02 10:50:57 -0700

    done ?

--- task3.py
+++ task3.py
@@ -14,13 +14,59 @@ w.title("sample")
 c = tk.Canvas(width=550,height=450,background="#cccccc",bd="2")
 c.pack()
 
+tile = 0 
 
-rec = c.create_rectangle(50,50,80,80,fill="#aa0000")
+def animate(): 
+    global tile
+    global sprite
+    ig = None
+    tile +=1
+    tile = tile%2
+    if tile: 
+        ig = win
+    else: 
+        ig = winb
+    c.itemconfig(sprite,image=ig)
+    w.after(200, animate)
 
+
+
+
+
+win = tk.PhotoImage(file="assets/sp2.png")
+winb = tk.PhotoImage(file="assets/sprite sheets.png")
+sprite = c.create_image(200,200, image = win)
+
+
+
+w.after(200, animate)
 
 def keyPress(e):
     print(e)
     print(e.keycode, e.keysym, e.x, e.y)
+
+    if e.keysym == "Up":
+        x=0
+        y=-5
+        x0 = c.coords(sprite) 
+        c.move(sprite,x,y)
+    if e.keysym == "Down":
+        x=0
+        y=5
+        x0 = c.coords(sprite)
+        c.move(sprite,x,y)
+    if e.keysym == "Left":
+        x=-5
+        y=0
+        x0 = c.coords(sprite)
+        c.move(sprite,x,y)
+    if e.keysym == "Right":
+        x=5
+        y=0
+        x0 = c.coords(sprite)
+        c.move(sprite,x,y)
+    
+
     
 w.bind("<Left>",keyPress)
 w.bind("<Right>",keyPress)
